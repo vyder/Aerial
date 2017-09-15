d7bac1c22ff9c25923ef2d2524b773e4825e94da
John Coates <john@johncoates.me>
2017-09-15 08:00:15 -0700

    Added CocoaPods, hope this doesn’t break Travis

---
+++ Aerial/Source/Headers/BridgingHeader.h
@@ -0,0 +1,10 @@
+//
+//  BridgingHeader.h
+//  Aerial
+//
+//  Created by John Coates on 9/14/17.
+//  Copyright © 2017 John Coates. All rights reserved.
+//
+
+#pragma once
+#import <VLCKit/VLCKit.h>
---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+pod 'VLCKit', '3.0.0a24'
+
+target 'Aerial'
+target 'AerialApp'
